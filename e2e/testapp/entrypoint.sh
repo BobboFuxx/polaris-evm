50354bc1b6c6fa9f239b08a44ffa26db9b2288d1
Bobbo Fuxx <134005319+BobboFuxx@users.noreply.github.com>
2024-06-25 21:59:21 -0700

    fix autistic spelling errors; entry point.sh
    Signed-off-by: Bobbo Fuxx <134005319+BobboFuxx@users.noreply.github.com>

--- e2e/testapp/entrypoint.sh
+++ e2e/testapp/entrypoint.sh
@@ -23,7 +23,7 @@ KEYS[0]="dev0"
 KEYS[1]="dev1"
 KEYS[2]="dev2"
 CHAINID="prysm-7007"
-MONIKER="evmestnet"
+MONIKER="evmtestnet"
 # Remember to change to other types of keyring like 'file' in-case exposing to outside world,
 # otherwise your balance will be wiped quickly
 # The keyring test does not require private key to steal tokens from you
